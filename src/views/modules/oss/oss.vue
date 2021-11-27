0bddc21bb00be3d4c1c60d04db997fa4d154264c
Naichuan Zhang <zhangnaichuan168@gmail.com>
2021-11-27 23:44:57 +0800

    添加其他模块页面

---
+++ src/views/modules/oss/oss.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'oss'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/modules/sys/user-add-or-update.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'user-add-or-update'
+}
+</script>
+
+<style scoped>
+
+</style>
